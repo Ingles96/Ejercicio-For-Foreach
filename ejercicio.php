3f946d17907ac43afba60628b9de13259db1b906
Ingles96 <josepingles96@gmail.com>
2016-12-01 09:18:13 +0100

    Update ejercicio.php

--- ejercicio.php
+++ ejercicio.php
@@ -15,18 +15,4 @@
         ?>
         </body>
 </html>
-include 'IA.php';
-$ial= new IA();
-$ial->setMapaCol(4);
-$ial->setMapaFil(3);
-$ial->setNaveHumana(,4,3);
-$ial->setNaveHumana(,3,2);
 
-public $numNavesHum="3";
-public function getnumNavesHum() {
-   return $this->numNavesHum;
- }
- public function setnumNavesHum($numNavesHum){
-   $this->numNavesHum = $numNavesHum;
-}
-++ para incrementar en 1 la variable
